f3f37f1f292bbb1ed62b97eadf418aa964a926b9
Roey Shtern <roeyshtern@gmail.com>
2021-10-07 19:37:50 +0300

    add players monitor

--- app/base/models.py
+++ app/base/models.py
@@ -4,11 +4,41 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from flask_login import UserMixin
-from sqlalchemy import Binary, Column, Integer, String
+from sqlalchemy import Binary, Column, Integer, String, Date
 
 from app import db, login_manager
 
 from app.base.util import hash_pass
+
+class PlayersMonitor(db.model):
+    __tablename__ = 'PlayersMonitor'
+
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer)
+    min_price = Column(Integer)
+    max_price = Column(Integer)
+    current_price = Column(Integer)
+    date_created = Column(Date)
+    last_updated = Column(Date)
+    last_alerted_time_min = Column(Date)
+    last_alerted_price_min = Column(Integer)
+    last_alerted_time_max = Column(Date)
+    last_alerted_price_max = Column(Integer)
+    user_id = Column(Integer)
+
+    def __init__(self, **kwargs):
+        for property, value in kwargs.items():
+            # depending on whether value is an iterable or not, we must
+            # unpack it's value (when **kwargs is request.form, some values
+            # will be a 1-element list)
+            if hasattr(value, '__iter__') and not isinstance(value, str):
+                # the ,= unpack of a singleton fails PEP8 (travis flake8 test)
+                value = value[0]
+
+            if property == 'password':
+                value = hash_pass( value ) # we need bytes here (not plain str)
+                
+            setattr(self, property, value)
 
 class User(db.Model, UserMixin):
 
